178065ef1110d39aff75729c150ae46a87c0e1db
BrokiDev <bryantramirez855@gmail.com>
2024-03-17 13:03:39 -0400

    [feature]- adding CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
 import { apiRoute } from './routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use('/api/shorturl',apiRoute);
